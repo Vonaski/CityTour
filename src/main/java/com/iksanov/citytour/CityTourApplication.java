9587b60accbdf17d46c347b638fbde37b7c4e627
refle <reflex.11.04.1999@gmail.com>
2026-08-07 19:01:49 +0500

    Configure PostgreSQL connection

--- src/main/java/com/iksanov/citytour/CityTourApplication.java
+++ src/main/java/com/iksanov/citytour/CityTourApplication.java
@@ -1,4 +1,4 @@
-package com.iksanov;
+package com.iksanov.citytour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/iksanov/citytour/CityTourApplicationTests.java
+++ src/test/java/com/iksanov/citytour/CityTourApplicationTests.java
@@ -1,4 +1,4 @@
-package com.iksanov;
+package com.iksanov.citytour;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
