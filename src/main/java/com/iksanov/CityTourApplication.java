a71a975123b03d4d0330dca75bf433a519baa3de
refle <reflex.11.04.1999@gmail.com>
2026-08-07 18:24:45 +0500

    init commit

---
+++ src/main/java/com/iksanov/CityTourApplication.java
@@ -0,0 +1,13 @@
+package com.iksanov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityTourApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CityTourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iksanov/CityTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iksanov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityTourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
