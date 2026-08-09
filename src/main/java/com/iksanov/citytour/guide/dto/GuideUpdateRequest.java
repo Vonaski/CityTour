137f8a670bd004f1678158f05ba9590a7fd584b3
refle <reflex.11.04.1999@gmail.com>
2026-08-09 15:51:55 +0500

    DTO for guide are constructed

---
+++ src/main/java/com/iksanov/citytour/guide/dto/GuideUpdateRequest.java
@@ -0,0 +1,32 @@
+package com.iksanov.citytour.guide.dto;
+
+import com.iksanov.citytour.guide.entity.Language;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+public class GuideUpdateRequest {
+
+    @NotBlank
+    @Size(min = 3, max = 120)
+    private String fullName;
+
+    @NotBlank
+    @Size(min = 9, max = 20)
+    private String phone;
+
+    @NotEmpty
+    private Set<Language> languages;
+
+    @Min(0)
+    private Integer experienceYears;
+
+    private Boolean active;
+}
